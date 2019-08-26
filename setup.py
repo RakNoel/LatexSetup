2ba798a6f1ee4dc28da2ba99036d712066b2a453
Oskar Leirvåg <oskar.leir@gmail.com>
2019-08-26 16:19:53 +0200

    More fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
+
+# Read Innput
 cCode = input("Enter course code (e.g. INF234): ")
 cName = input("Enter cource name (e.g. Advanced alg.): ")
 dName = input("Enter Document title (e.g. Obligatory ass. 1): ")
 
-innFile = open("uib_frontpage.tex", "r+")
+# Static var
+fileName = "uib_frontpage.tex"
 
+# Change text
+innFile = open(fileName, "r+")
 old = innFile.read()
 fix = old.replace("--#1--", cCode).replace("--#2--", cName).replace("--#3--", dName)
-
-innFile.write(fix)
 innFile.close()
+
+# Save file
+outFile = open(fileName, "w+")
+outFile.write(fix)
+outFile.close()
